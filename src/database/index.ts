5be8847d07a627a497979184148f2e534b9930ed
Kasu0831 <kukuaice@gmail.com>
2024-02-09 14:36:15 +0900

    mod2

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+  const url = process.env.NEXT_PUBLIC_DB_URL as string
   try {
     await mongoose.connect(
-      `mongodb+srv://kazz:${process.env.NEXT_PUBLIC_DB_PASSWORD}@cluster0.6aspsru.mongodb.net/`
+      url
     );
     console.log("DBに接続しました");
   } catch (err) {
